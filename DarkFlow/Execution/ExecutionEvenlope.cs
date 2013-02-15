a6eb150ec3dfa6d2b7a4e2d93c36863e6a75f073
j.soldatenkov <yurysold@googlemail.com>
2013-02-16 00:00:46 +0400

    Added ExecutionEnvelope

---
+++ DarkFlow/Execution/ExecutionEvenlope.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Codestellation.DarkFlow.Misc;
+using NLog;
+
+namespace Codestellation.DarkFlow.Execution
+{
+    public class ExecutionEnvelope
+    {
+        private readonly ITaskReleaser _releaser;
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public ITask Task { get; private set; }
+        
+        public Guid Id { get; private set; }
+
+        internal Action AfterExecute { get; set; }
+
+        public ExecutionEnvelope(ITask task, ITaskReleaser releaser) : this(Guid.NewGuid(), task, releaser)
+        {
+            
+        }
+
+        internal ExecutionEnvelope(Guid id, ITask task, ITaskReleaser releaser)
+        {
+            Contract.Require(task != null, "task != null");
+            Contract.Require(id != Guid.Empty, "id != Guid.Empty");
+            Contract.Require(releaser != null, "releaser != null");
+
+            Task = task;
+            Id = id;
+            _releaser = releaser;
+        }
+
+        public void ExecuteTask()
+        {
+            Contract.Require(AfterExecute != null, "AfterExecute != null");
+            try
+            {
+                Task.Execute();
+            }
+            catch (Exception ex)
+            {
+                if (Logger.IsErrorEnabled)
+                {
+                    Logger.ErrorException("Task failed.", ex);
+                }
+            }
+            finally
+            {
+                _releaser.Release(Task);
+                AfterExecute();
+            }
+        }
+    }
+}
