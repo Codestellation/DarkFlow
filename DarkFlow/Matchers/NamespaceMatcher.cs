bd7c2b1f650fc4a3b2f2db591fdec7743636c7c7
j.soldatenkov <yurysold@googlemail.com>
2013-03-13 23:28:45 +0400

    Implemented caching for matchers

--- DarkFlow/Matchers/NamespaceMatcher.cs
+++ DarkFlow/Matchers/NamespaceMatcher.cs
@@ -1,4 +1,6 @@
-﻿using Codestellation.DarkFlow.Misc;
+﻿using System;
+using System.Collections.Generic;
+using Codestellation.DarkFlow.Misc;
 
 namespace Codestellation.DarkFlow.Matchers
 {
@@ -6,16 +8,18 @@ namespace Codestellation.DarkFlow.Matchers
     {
         private readonly string _ns;
         private readonly string _queueName;
+        private readonly Dictionary<Type, bool> _matchedCache;
         public const string All = "*";
-
+        
 
         public NamespaceMatcher(string @namespace, string queueName)
         {
             Contract.Require(!string.IsNullOrWhiteSpace(@namespace), "!string.IsNullOrWhiteSpace(@namespace)");
             Contract.Require(!string.IsNullOrWhiteSpace(queueName), "!string.IsNullOrWhiteSpace(queueName)");
             
-            _ns = @namespace.Replace("*", string.Empty);
+            _ns = @namespace.Replace(All, string.Empty);
             _queueName = queueName;
+            _matchedCache = new Dictionary<Type, bool>();
         }
 
         public string QueueName
@@ -27,7 +31,14 @@ namespace Codestellation.DarkFlow.Matchers
         {
             Contract.Require(task != null, "task != null");
 
-            return task.GetType().FullName.StartsWith(_ns) || _ns == All;
+            var type = task.GetType();
+            var result = _matchedCache.GetOrAddThreadSafe(type, () => Match(type));
+            return result;
+        }
+
+        private bool Match(Type type)
+        {
+            return type.FullName.StartsWith(_ns) || _ns == All;
         }
     }
 }
---
+++ DarkFlow/Misc/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codestellation.DarkFlow.Misc
+{
+    internal static class Extensions
+    {
+        public static TValue GetOrAddThreadSafe<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key, Func<TValue> valueBuilder)
+        {
+            TValue result;
+
+            if (self.TryGetValue(key, out result))
+            {
+                return result;
+            }
+
+            lock (self)
+            {
+                if (self.TryGetValue(key, out result))
+                {
+                    return result;
+                }
+                result = valueBuilder();
+                self[key] = result;
+            }
+
+            return result;
+        }
+    }
+}
