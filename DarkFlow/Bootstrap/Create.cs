7636311f27422d2834820965a23fbbdf996a375b
Jury Soldatenkov <yurysold@googlemail.com>
2013-03-25 23:45:37 +0400

    Implemented rude xml-configuration parser (close #25)

--- DarkFlow/Bootstrap/Create.cs
+++ DarkFlow/Bootstrap/Create.cs
@@ -1,10 +1,83 @@
-﻿namespace Codestellation.DarkFlow.Bootstrap
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Codestellation.DarkFlow.Config;
+using Codestellation.DarkFlow.Database;
+using Codestellation.DarkFlow.Execution;
+using Codestellation.DarkFlow.Matchers;
+using SimpleConfig;
+
+namespace Codestellation.DarkFlow.Bootstrap
 {
     public static class Create
     {
-        public static int Threads(this int self)
+        public static IExecutor FromXmlConfig()
         {
-            return self;
+            var config = Configuration.Load<DarkFlowConfiguration>("darkFlow");
+
+            var queues = new TaskQueue[config.Executors.Count];
+
+            var database = new ManagedEsentDatabase();
+
+
+            var persisterMatcher = BuildMatcher(config.Persistence.Matchers);
+
+            var persister = new Persister(database, persisterMatcher);
+
+            for (int i = 0; i < config.Executors.Count; i++)
+            {
+                queues[i] = new TaskQueue(config.Executors[i], persister);
+            }
+
+            var routerMatcher = BuildMatcher(config.Routes);
+
+            var router = new TaskRouter(routerMatcher, queues);
+            
+            var dispatcher = new TaskDispatcher((byte) config.Dispatcher.MaxConcurrency, queues);
+
+            var result = new Executor(router, dispatcher, DefaultReleaser.Instance);
+
+            return result;
+        }
+
+        //note - usage of default queue name is merely hach to bring things to work without refactoting matchers. (Persister does not need to know queue name)
+        private static IMatcher BuildMatcher(List<MatcherSettings> matcherSettings, string executorName = "no-matter")
+        {
+            var matchers = new List<IMatcher>();
+
+            foreach (var matcherSettingse in matcherSettings)
+            {
+                IMatcher matcher;
+                switch (matcherSettingse.Type)
+                {
+                    case "namespace" :
+                        matcher = new NamespaceMatcher(matcherSettingse.RouteTo ?? executorName, matcherSettingse.Mask);
+                        matchers.Add(matcher);
+                        break;
+                    case "attribute" :
+                        var assembly = Assembly.Load(matcherSettingse.Assembly);
+                        var attributeNames = matcherSettingse.Attributes.Split(',').Select(x => x.Trim()).ToArray();
+
+                        var attributeTypes = assembly.GetTypes()
+                                                    .Where(
+                                                        x =>
+                                                        attributeNames.Any(
+                                                            attrib =>
+                                                            attrib.Equals(x.Name,
+                                                                          StringComparison.InvariantCultureIgnoreCase)))
+                                                    .ToArray();
+
+                        matcher = new AttributeMatcher(executorName, attributeTypes);
+                        matchers.Add(matcher);
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+
+            var result = new AggregateMatcher(matchers.ToArray());
+            return result;
         }
     }
 }
--- Tests/Core/Bootstrap/BootstrapperTests.cs
+++ Tests/Core/Bootstrap/BootstrapperTests.cs
@@ -1,4 +1,5 @@
-﻿using Codestellation.DarkFlow.Bootstrap;
+﻿using System;
+using Codestellation.DarkFlow.Bootstrap;
 using NUnit.Framework;
 
 namespace Codestellation.DarkFlow.Tests.Core.Bootstrap
@@ -6,6 +7,14 @@ namespace Codestellation.DarkFlow.Tests.Core.Bootstrap
     [TestFixture]
     public class BootstrapperTests
     {
-        
+        [Test]
+        public void Rude_test_for_reading_config()
+        {
+            var executor = Create.FromXmlConfig();
+
+            Assert.That(executor, Is.Not.Null);
+
+            ((IDisposable)executor).Dispose();
+        }
     }
 }
