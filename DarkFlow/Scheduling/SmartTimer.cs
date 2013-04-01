e88388ceef325a6bb7803a135c84c4e586b3bfd4
Jury Soldatenkov <yurysold@googlemail.com>
2013-04-01 16:41:38 +0400

    Fixed bug with ArgumentOutOfRange in SmartTimer (close #26)

--- DarkFlow/Scheduling/SmartTimer.cs
+++ DarkFlow/Scheduling/SmartTimer.cs
@@ -1,18 +1,21 @@
 ﻿using System;
-using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Threading;
 using Codestellation.DarkFlow.Misc;
+using NLog;
 
 namespace Codestellation.DarkFlow.Scheduling
 {
     public class SmartTimer : ITimer
     {
-        private readonly ConcurrentDictionary<DateTimeOffset, Timer> _timers;
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private readonly Dictionary<DateTimeOffset, Timer> _timers;
         private IClock _clock;
+        private Action<DateTimeOffset> _callback;
 
         public SmartTimer(IClock clock)
         {
-            _timers = new ConcurrentDictionary<DateTimeOffset, Timer>();
+            _timers = new Dictionary<DateTimeOffset, Timer>();
             _clock = clock;
         }
 
@@ -30,33 +33,63 @@ namespace Codestellation.DarkFlow.Scheduling
 
         public void CallbackAt(DateTimeOffset startAt)
         {
-            _timers.GetOrAdd(startAt, BuildTimer);
+            var timer = BuildTimer(startAt);
+            if (timer == null) return;
+            lock (_timers)
+            {
+                _timers[startAt] = timer;
+            }
         }
 
-        public Action<DateTimeOffset> Callback { get; set; }
+        public Action<DateTimeOffset> Callback
+        {
+            get { return _callback; }
+            set
+            {
+                _callback = value;
+                Thread.MemoryBarrier();
+            }
+        }
 
         private Timer BuildTimer(DateTimeOffset startAt)
         {
-            var dueTime = startAt - _clock.Now;
-            return new Timer(OnTimerCallback, startAt, Convert.ToInt64(dueTime.TotalMilliseconds), -1);
+            var now = _clock.Now;
+            var dueTime = startAt - now;
+            var dueTimeInMilliseconds = Convert.ToInt64(dueTime.TotalMilliseconds);
+
+            //TODO remove timer creation. Do call to callback immediately.
+            if (dueTimeInMilliseconds < 0)
+            {
+                if (Logger.IsWarnEnabled)
+                {
+                    Logger.Warn("Attempt to start timer in past: now '{0}', StartAt '{1}', dueTime {2}", now, startAt, dueTime);
+                }
+                Callback(startAt);
+                return null;
+            }
+
+            return new Timer(OnTimerCallback, startAt, dueTimeInMilliseconds, -1);
         }
 
         private void OnTimerCallback(object state)
         {
-            var startAt = (DateTimeOffset) state;
+            var startAt = (DateTimeOffset)state;
             try
             {
                 var callback = Callback;
+                Thread.MemoryBarrier();
                 if (callback == null) return;
                 callback(startAt);
             }
             finally
             {
                 Timer timer;
-                if (_timers.TryRemove(startAt, out timer))
+                lock (_timers)
                 {
-                    timer.Dispose();
+                    timer = _timers[startAt];
+                    _timers.Remove(startAt);
                 }
+                timer.Dispose();
             }
         }
     }
---
+++ Tests/Core/Scheduling/SmartTimerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using Codestellation.DarkFlow.Misc;
+using Codestellation.DarkFlow.Scheduling;
+using NUnit.Framework;
+
+namespace Codestellation.DarkFlow.Tests.Core.Scheduling
+{
+    [TestFixture]
+    public class SmartTimerTests
+    {
+        private ManualResetEventSlim _callbackInvoked;
+
+        [Test]
+        public void Should_callback_if_due_date_missed()
+        {
+            _callbackInvoked = new ManualResetEventSlim(false);
+
+            Thread.MemoryBarrier();
+
+            var now = new DateTimeOffset(2010, 01, 01, 10, 0, 0, TimeSpan.FromHours(1));
+
+            var clock = new TestClock(now);
+            var timer = new SmartTimer(clock) {Callback = OnTimerCallback};
+            
+            timer.CallbackAt(now.AddHours(-1));
+
+            _callbackInvoked.Wait(TimeSpan.FromSeconds(5));
+
+            Assert.That(_callbackInvoked.IsSet, Is.True, "Callback was not invoked");
+
+        }
+
+        private void OnTimerCallback(DateTimeOffset obj)
+        {
+            _callbackInvoked.Set();
+        }
+    }
+}
