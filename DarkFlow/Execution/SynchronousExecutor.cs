b88cc24f6f7b82f509a92e84207c449e02b3d72c
Jury Soldatenkov <yurysold@googlemail.com>
2013-04-01 17:56:52 +0400

    Improved SynchronousExecutor

--- DarkFlow/Execution/SynchronousExecutor.cs
+++ DarkFlow/Execution/SynchronousExecutor.cs
@@ -1,10 +1,38 @@
-﻿namespace Codestellation.DarkFlow.Execution
+﻿using System;
+
+namespace Codestellation.DarkFlow.Execution
 {
-    public class SynchronousExecutor : IExecutor
+    public class SynchronousExecutor : IExecutor, IExecutorImplementation
     {
+        private readonly string _name;
+        public const string DefaultName = "Sync";
+
+        public SynchronousExecutor() : this(DefaultName)
+        {
+        }
+
+        public SynchronousExecutor(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Argument name should be not null not empty string", "name");
+            }
+            _name = name;
+        }
+
         public void Execute(ITask task)
         {
             task.Execute();
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public void Enqueue(ExecutionEnvelope envelope)
+        {
+            Execute(envelope.Task);
         }
     }
 }
