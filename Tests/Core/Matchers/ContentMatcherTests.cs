64dfefc4148af6f52a9226e25fccda5ba79607f7
Jury Soldatenkov <yurysold@googlemail.com>
2013-04-06 23:58:00 +0400

    Implemeted ContentMatcher (relates to #29)

---
+++ Tests/Core/Matchers/ContentMatcherTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Codestellation.DarkFlow.Matchers;
+using NUnit.Framework;
+
+namespace Codestellation.DarkFlow.Tests.Core.Matchers
+{
+    [TestFixture]
+    public class ContentMatcherTests
+    {
+        private class ContentTestTask : ITask
+        {
+            public int Count;
+
+            public string Name { get; set; }
+
+            public void Execute()
+            {
+                
+            }
+        }
+        
+        [TearDown]
+        public void TearDown()
+        {
+            Environment.SetEnvironmentVariable("envvar", null);
+
+        }
+
+        [Test]
+        public void Should_build_corrent_string()
+        {
+            var task = new ContentTestTask {Name = "Test" , Count = 7 };
+
+            Environment.SetEnvironmentVariable("envvar", "Ok");
+
+            var matcher = new ContentMatcher("executor-{Name}-{Count}-{env:enVvaR}");
+
+            var result = matcher.TryMatch(task);
+
+            Assert.That(result.Value, Is.EqualTo("executor-Test-7-Ok"));
+        }
+    }
+}
