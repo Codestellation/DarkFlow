caa25040c600120083611f7d555adfd8a0e7767b
Jury Soldatenkov <yurysold@gmail.com>
2013-10-09 12:31:55 +0400

    Added Start method extenstions

--- DarkFlow/Triggers/Start.cs
+++ DarkFlow/Triggers/Start.cs
@@ -6,6 +6,29 @@ namespace Codestellation.DarkFlow.Triggers
     {
         public static DateTime? Immediately = null;
 
+        public static DateTime At(TimeSpan timeOfDay, DateTimeKind dateTimeKind)
+        {
+            if (timeOfDay < TimeSpan.FromSeconds(0) || TimeSpan.FromDays(1) < timeOfDay)
+            {
+                throw new ArgumentOutOfRangeException("timeOfDay", "Time of day should be in range from 0 up to 24 hours");
+            }
+            if (dateTimeKind == DateTimeKind.Unspecified)
+            {
+                throw new ArgumentOutOfRangeException("dateTimeKind", "DateTimeKind should be UTC or Local");
+            }
+
+            var currentDateTime = dateTimeKind == DateTimeKind.Utc ? DateTime.UtcNow : DateTime.Now;
+
+            var result = currentDateTime.Date.Add(timeOfDay);
+
+            //Missed in current day. Plan it tomorrow
+            if (currentDateTime.TimeOfDay > timeOfDay)
+            {
+                result = result.AddDays(1);
+            }
+
+            return result;
+        }
     }
 
     public static class Repeat
